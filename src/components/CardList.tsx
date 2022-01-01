84f139d065b0021c2edad4850031cb97d7853de7
Lucas Freitas <80073416+lucasfreitass10@users.noreply.github.com>
2022-01-01 15:34:22 -0300

    first commit

---
+++ src/components/CardList.tsx
@@ -0,0 +1,42 @@
+import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Card } from './Card';
+import { ModalViewImage } from './Modal/ViewImage';
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+interface CardsProps {
+  cards: Card[];
+}
+export function CardList({ cards }: CardsProps): JSX.Element {
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
+  const handleViewImage = (url: string): void => {
+    setSelectedImageUrl(url);
+    onOpen();
+  };
+  return (
+    <>
+      {
+        <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+          {cards.map(card => {
+            return (
+              <Card key={card.id} data={card} viewImage={handleViewImage} />
+            );
+          })}
+        </SimpleGrid>
+      }
+      {
+        <ModalViewImage
+          imgUrl={selectedImageUrl}
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      }
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import { Button, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { Header } from '../components/Header';
+import { CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+export default function Home(): JSX.Element {
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'images',
+    async ({ pageParam = null }): Promise<GetImagesResponse> => {
+      const response = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
+  );
+  const formattedData = useMemo(() => {
+    return data?.pages.flatMap(pageData => pageData.data.flat());
+  }, [data]);
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+  return (
+    <>
+      <Header />
+      <Box maxW={1120} px={20} mx="auto" my={20}>
+        <CardList cards={formattedData} />
+        {
+          hasNextPage && (
+            <Button onClick={() => fetchNextPage()} marginTop="2.5rem">
+              {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+            </Button>
+          )
+        }
+      </Box>
+    </>
+  );
+}
